a2665476f48fa77cca52dbea2d76b19674c6a1ee
Karolina Simcic <kgsimcic@gmail.com>
2025-03-31 11:45:34 -0400

    Accidentally deleted the controller files. Fixed this by replacing and renaming the controllers to follow the api conventions.

---
+++ DotNetProject10/Controllers/NotesController.cs
@@ -0,0 +1,33 @@
+﻿using PatientMicroservice.Services;
+using Microsoft.AspNetCore.Mvc;
+using PatientMicroservice.Models;
+
+namespace PatientMicroservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+        private readonly ILogger<NotesController> _logger;
+
+        public NotesController(INoteService noteService, ILogger<NotesController> logger)
+        {
+            _noteService = noteService;
+            _logger = logger;
+        }
+
+        [HttpGet("Notes/{patientId}")]
+        public async Task<ActionResult> GetNotes(int patientId)
+        {
+            return Ok(await _noteService.GetNotes(patientId.ToString()));
+        }
+
+        [HttpPost("Notes")]
+        public async Task<IActionResult> CreateNote([FromBody] DoctorNoteModel noteModel)
+        {
+            await _noteService.Create(noteModel);
+            return Created();
+        }
+    }
+}
---
+++ DotNetProject10/Controllers/PatientsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientMicroservice.Entities;
+using PatientMicroservice.Models;
+using PatientMicroservice.Services;
+
+namespace PatientMicroservice.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PatientsController(IPatientService patientService, ILogger<PatientsController> logger, IReportService reportService) : ControllerBase
+    {
+        private readonly ILogger<PatientsController> _logger = logger;
+        private readonly IPatientService _patientService = patientService;
+        private readonly IReportService _reportService = reportService;
+
+        [HttpGet()]
+        public async Task<ActionResult> GetPatients()
+        {
+            return Ok(await _patientService.GetPatients());
+        }
+
+        [HttpGet("{patientId}")]
+        public async Task<IActionResult> GetById(int patientId)
+        {
+            return Ok(await _patientService.GetPatientById(patientId));
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> CreatePatient([FromBody] PatientModel patientModel)
+        {
+            int patientId = await _patientService.CreatePatient(patientModel);
+            return Ok(patientId);
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> UpdatePatient([FromBody] PatientModel patientModel, int Id)
+        {
+            if (patientModel.Id != Id)
+            {
+                return BadRequest("Id of request does not match patient info passed.");
+            }
+            else
+            {
+                await _patientService.UpdatePatient(patientModel);
+                return Created();
+            }
+        }
+
+        [HttpGet("Report/{patientId}")]
+        public async Task<ActionResult> GenerateReport(int patientId)
+        {
+            string result = await _reportService.GenerateReport(patientId);
+            Console.WriteLine(result);
+            return Ok(result);
+        }
+    }
+}
